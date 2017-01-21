ca7ca29d2419a7e64b71d2b64811fef1deea1328
Lukas Klingberg <lukas@klingberg.se>
2017-01-21 09:21:14 +0100

    removed ignored files

--- bin/app/app.js
+++
@@ -1,6 +0,0 @@
-const uiHandler = new UiHandler();
-const option = '';
-window.onload = () => {
-    uiHandler.setContainer(document.getElementById('content-container'));
-    uiHandler.setupStartState();
-};
